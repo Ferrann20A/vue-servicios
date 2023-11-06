6c03b42eb21f4d62958a2ce41d967c77eb901686
Ferrann20A <alumno.398783@ies-azarquiel.es>
2023-11-06 10:53:16 +0100

    commit

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="container">
         <h1>Home</h1>
+        <h2>
+            {{mensaje}}
+        </h2>
     </div>
 </template>
 
 <script>
+    import ServiceEjemplo from '../services/ServiceEjemplo'
+    //Para utilizar un servicio debemos crear una constante que sea un objeto de la clase service
+    const service = new ServiceEjemplo();
+
     export default{
-        name:"HomeComponent"
+        name:"HomeComponent",
+        data(){
+            return{
+                mensaje:""
+            }
+        },
+        mounted(){
+            this.mensaje = service.getSaludo("Alumno");
+        }
     }
 </script>
 
